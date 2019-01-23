f7bdd3147e6d43407549c88a84487c405fc3959a
mauget <mauget@gmail.com>
2019-01-23 14:51:37 -0500

    removed dead header div from root

--- src/index.js
+++ src/index.js
@@ -20,17 +20,20 @@ import "mdbreact/dist/css/mdb.css";
 import './styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import FooterPanel from './footer';
 import { Provider } from 'react-redux';
-import configureStore from './configureStore';
+import { store } from './configureStore';
 
 const element = (
-    <Provider id="wrapper" store={configureStore()}>
-        <div id="header"></div>
-        <div id="content"><App/></div>
-        <div id="footer"><FooterPanel/></div>
-    </Provider>
+    <BrowserRouter id="wrapper">
+        <Provider store={store}>
+            {/*<div id="header"></div>*/}
+            <div id="content"><App/></div>
+            <div id="footer"><FooterPanel/></div>
+        </Provider>
+    </BrowserRouter>
 );
 
 ReactDOM.render(
